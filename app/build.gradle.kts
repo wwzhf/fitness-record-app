fde622f8c460ecdcd17dfbb959518f8fca730372
wwzhf <wwzhf@users.noreply.github.com>
2026-08-30 11:58:26 +0800

    chore: version 1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.wc.workout"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.2.2"
+        versionCode = 15
+        versionName = "1.2.3"
     }
 
     buildTypes {
